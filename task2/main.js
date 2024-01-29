b2c2f3a251b0ff04fac2911690ae5f5b3e37b76e
Hemmich <dmitrij.kaxno@bk.ru>
2024-01-29 13:47:39 +0300

    написал программу которая определяет тип одежды под температуру

--- task2/main.js
+++ task2/main.js
@@ -1,1 +1,11 @@
-бабах
+let temperature = 25;
+
+if (temperature > 30) {
+  console.log("Вы можете надеть легкую одежду");
+} else if (temperature > 20) {
+  console.log("Нужно надеть теплую одежду");
+} else if (temperature < 10) {
+  console.log("Сегодня холодно, нужно надевать зимнюю одежду");
+} else {
+    console.log("температура нормальная")
+}
